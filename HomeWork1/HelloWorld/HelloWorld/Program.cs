bb7723b96d2c283ab0ec1de2b54a4d4aa38a3fde
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-02-16 19:52:36 +0100

    Created and finished first homework

---
+++ HomeWork1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сейчас {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
