187eb55446780355250bde5eb0e7579c33296cf2
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-02-17 16:39:42 +0100

    Started work on additional homework

---
+++ HomeWorks/lesson1/HomeWorkAdd/Password/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой пароль");
+            string userPass = (Console.ReadLine());
+            //char[] letters = userPass.ToCharArray();
+            //char letters = userPass[0];
+
+            for (int i = 0; i < userPass.Length - 1; i++)
+            {
+                if (userPass[i] == userPass[i + 1])
+                {
+                    Console.WriteLine("Одинаковые символы не должны идти подряд");
+                    break;
+                }
+                else if (userPass.Length > 6)
+                {
+                    Console.WriteLine("Пароль слишком длинный!");
+                    break;
+                }
+                else if (userPass.Length <4)
+                {
+                    Console.WriteLine("Пароль слишком короткий");
+                    break;
+                }
+                else
+                {
+
+                }
+                
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
