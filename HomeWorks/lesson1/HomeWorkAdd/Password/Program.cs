ed7a26d52130dbe328f9d0088f87c816e3f598a3
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-02-18 10:13:26 +0100

    Updated additioanl homework

--- HomeWorks/lesson1/HomeWorkAdd/Password/Program.cs
+++ HomeWorks/lesson1/HomeWorkAdd/Password/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Password
 {
@@ -8,32 +12,25 @@ namespace Password
         {
             Console.WriteLine("Введите свой пароль");
             string userPass = (Console.ReadLine());
-            //char[] letters = userPass.ToCharArray();
-            //char letters = userPass[0];
-
-            for (int i = 0; i < userPass.Length - 1; i++)
-            {
-                if (userPass[i] == userPass[i + 1])
+            int[] password = userPass.ToCharArray().Select(userPass => int.Parse(userPass.ToString())).ToArray();
+            for (int j = 0; j <= userPass.Length; j++)
                 {
-                    Console.WriteLine("Одинаковые символы не должны идти подряд");
-                    break;
+                    //int[] password = userPass.ToCharArray().Select(i => int.Parse(i.ToString())).ToArray();
+                    if (password[j] != password[j + 1] && userPass.Length <= 6 && userPass.Length >= 4)
+                    {
+                        continue;
+                    }
+                    else if (j >= userPass.Length)                
+                    {
+                        Console.WriteLine("Пароль подходит!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пароль не подходит!");
+                        break;
+                    }
                 }
-                else if (userPass.Length > 6)
-                {
-                    Console.WriteLine("Пароль слишком длинный!");
-                    break;
-                }
-                else if (userPass.Length <4)
-                {
-                    Console.WriteLine("Пароль слишком короткий");
-                    break;
-                }
-                else
-                {
-
-                }
-                
-            }
             Console.ReadKey();
             
         }
