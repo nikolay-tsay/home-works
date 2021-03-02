a1ed890a40cfddbdbbd2b80930ec294afe4472af
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-03-02 20:06:28 +0100

    Started work on homework for lesson 4

---
+++ HomeWorks/lesson4/lesson4.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
