c88c60bfc5d5aa9db32e43e171353f60b55ca1e9
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-03-03 13:44:27 +0100

    finished homework for lesson 4

--- HomeWorks/lesson4/lesson4.1/Program.cs
+++ HomeWorks/lesson4/lesson4.1/Program.cs
@@ -2,11 +2,35 @@
 
 namespace lesson4._1
 {
+    /*Написать метод GetFullName(string firstName, string lastName, string patronymic),
+     принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО. 
+     Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.*/
     class Program
     {
+        static void WriteFullName()
+        {
+            Console.Write("Введите ваше имя:");
+            string firstName = Console.ReadLine();
+            
+            Console.Write("Введите вашу фамилию:");
+            string lastName = Console.ReadLine();
+            
+            Console.Write("Введите ваше отчество:");
+            string patronymic = Console.ReadLine();
+
+            Console.WriteLine($"ФИО: {GetFullName(firstName, lastName, patronymic)}");
+
+        }
+        
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return new string($"{lastName} {firstName} {patronymic}");
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteFullName();
+            Console.ReadLine();
         }
     }
 }
--- HomeWorks/lesson4/lesson4.2/Program.cs
+++ HomeWorks/lesson4/lesson4.2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Linq;
 
 namespace lesson4._2
 {
+    /*Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом,
+     и возвращающую число — сумму всех чисел в строке.
+     Ввести данные с клавиатуры и вывести результат на экран.*/
     class Program
     {
+        static void SumOfAll(string str)
+        {
+            int result = 0;
+            var array =str.Split(' ').Select(Int32.Parse).ToArray();
+            //Так и не понял, как тут обработать исключение, если ввести не числа.
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            
+            Console.WriteLine($"Сумма всех чисел: {result}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите строку чисел и разделите их пробелами");
+            string userStr = Console.ReadLine();
+            
+            SumOfAll(userStr);
+
+            Console.ReadLine();
         }
     }
 }
