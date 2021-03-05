d498704186ad361263f3da71f3a1b6cdb7580146
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-03-05 15:14:34 +0100

    started work on homework for lesson 5

---
+++ HomeWorks/lesson5/lesson5.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson5._4
+{
+    //Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
