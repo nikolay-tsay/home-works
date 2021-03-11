78271cdf8f1f5862dc33678c7d9c29cfdcba7e40
Nikolay Tsay <tsay.nikolay99@gmail.com>
2021-03-11 19:23:35 +0100

    Built solution

---
+++ HomeWorks/lesson7/lesson7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введит сообщение для теста");
+            string massage = Console.ReadLine();
+            Console.Clear();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Вывод {i}:{massage}");
+            }
+        }
+    }
+}
